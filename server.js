4ced0d43571dbff2c3a35b1860588c23ff3bc04a
Zack Jameson <zacharymjameson@gmail.com>
2021-03-26 16:44:29 -0400

    validation

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+
+console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.send("Hello, world!");
-});
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`,
+];
+
+app.use(function validateBearerToken(req, res, next) {
+    console.log('validate bearer token middleware')
+
+    next()
+})
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+app.get("/types", handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  res.send("Hello, Pokemon!");
+}
+
+app.get("/pokemon", handleGetPokemon);
 
 const PORT = 8000;
 
