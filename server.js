9a8afd205e0e9403688daccad46cb0fe35ff3635
Zack Jameson <zacharymjameson@gmail.com>
2021-03-28 08:48:24 -0400

    hide errors and add procfile

--- server.js
+++ server.js
@@ -5,19 +5,16 @@ const helmet = require("helmet");
 const cors = require("cors");
 const POKEDEX = require("./pokedex.json");
 
-console.log(process.env.API_TOKEN);
-
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-
-  console.log("validate bearer token middleware");
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
@@ -68,8 +65,18 @@ app.get("/pokemon", function handleGetPokemon(req, res) {
   res.json(response);
 });
 
+app.use((error, req, res, next) => {
+  let response 
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Gotta catch 'em all in http://localhost:${PORT}`);
+  console.log(`Gotta catch 'em all`);
 });
